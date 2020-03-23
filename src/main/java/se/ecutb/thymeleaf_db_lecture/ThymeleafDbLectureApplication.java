58d8fff73bc5f68836413240b99083d524811871
Erik Svensson <eosvensson@gmail.com>
2020-03-23 08:27:49 +0100

    Uploaded

---
+++ src/main/java/se/ecutb/thymeleaf_db_lecture/ThymeleafDbLectureApplication.java
@@ -0,0 +1,13 @@
+package se.ecutb.thymeleaf_db_lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafDbLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafDbLectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/ecutb/thymeleaf_db_lecture/ThymeleafDbLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package se.ecutb.thymeleaf_db_lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafDbLectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
